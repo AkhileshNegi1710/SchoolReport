ff4e52541d5151eb1f0e517d34377c54bb1892bc
Akhilesh Negi <79513645+AkhileshNegi1710@users.noreply.github.com>
2023-12-11 20:11:47 +0530

    Delete StudentReport directory

--- StudentReport/studentWelcome/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-# creating html form using python
-class contactForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    email = forms.EmailField()
-    content = forms.CharField(widget=forms.Textarea())
